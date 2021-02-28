9634ff9dcf93f9a65c9e47b2ed9bde3b81c63c3a
Alice Wang <yuchen@Alices-MacBook-Pro.local>
2021-02-28 18:04:31 +0800

    First commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+var cors = require('cors')
+
+app.use(cors())
+app.use(fileUpload());
+
+app.use(express.static('client/public'));
+
+// Upload Endpoint
+app.post('/upload', (req, res) => {
+    if(req.files === null) {
+        return res.status(400).json({ msg: 'No file uploaded' });
+    }
+
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+        if(err) {
+            console.error(err);
+            return res.status(500).send(err);
+        }
+
+        //const file_url = URL.createObjectURL(file);
+
+
+        res.json({ fileName: file.name, filePath: `${__dirname}/client/public/uploads/${file.name}` });
+    });
+
+});
+
+app.listen(5000, () => console.log('Server Started...'));
